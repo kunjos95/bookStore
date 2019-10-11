2ead84f460af48ed86ac9faa583361b4552479ae
Josué Isael Vázquez Nájera <32659829+kunjos95@users.noreply.github.com>
2019-10-11 12:39:01 -0500

    Version1
    Version 1 del proyecto bookStore con Angular y firebase

---
+++ bookStore/src/app/services/data-api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataApiService {
+
+  constructor() { }
+
+  registerUser(){}
+  loginEmailUser(){}
+  loginFacebookUser(){}
+  loginGoogleUser(){}
+  logoutUser(){}
+}
---
+++ bookStore/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyA9PKuqp9HyMwJu9jSlRi6M8-jx_aUn5SM",
+    authDomain: "bookstore-8b936.firebaseapp.com",
+    databaseURL: "https://bookstore-8b936.firebaseio.com",
+    projectId: "bookstore-8b936",
+    storageBucket: "bookstore-8b936.appspot.com",
+    messagingSenderId: "971993689855",
+    appId: "1:971993689855:web:3a6d40429a25c3df9426ca",
+    measurementId: "G-RTJC5RQ3YJ"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
